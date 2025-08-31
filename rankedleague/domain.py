381fc16513f5e7011c30bd663b1b44b5f3c29b0d
David Campey <campey@gmail.com>
2025-08-31 16:01:47 +0200

    Add Result class to domain, with logic for a winner

--- rankedleague/domain.py
+++ rankedleague/domain.py
@@ -13,3 +13,21 @@ class Team:
     def __repr__(self):
         return f"Team({self.name})"
     
+
+class Result:
+    def __init__(self, team1: Team, score1: int, team2: Team, score2: int):
+        self.team1 = team1
+        self.score1 = score1
+        self.team2 = team2
+        self.score2 = score2
+
+    def __repr__(self):
+        return f"Result({self.team1} {self.score1}, {self.team2} {self.score2})"
+    
+    def winner(self):
+        if self.score1 > self.score2:
+            return self.team1
+        elif self.score2 > self.score1:
+            return self.team2
+        else:
+            return None
--- tests/test_ranking.py
+++ tests/test_ranking.py
@@ -1,4 +1,4 @@
-from rankedleague.rankedleague.domain import Team
+from rankedleague.rankedleague.domain import Team, Result
 
 def test_team_init():
     team = Team("Lions")
@@ -7,19 +7,47 @@ def test_team_init():
 
 def test_team_equality():
     team1 = Team("Lions")
-    team2 = Team("Lions")
-    team3 = Team("Grouches")
-    
+    team2 = Team("Lions")    
     assert team1 == team2
+    
+def test_team_inequality():
+    team1 = Team("Lions")   
+    team3 = Team("Grouches")
     assert team1 != team3
+
+def test_team_not_equals_string():
+    team1 = Team("Lions")
     assert team1 != "NotATeam"
 
 def test_team_hash():
     team1 = Team("Lions")
     team2 = Team("Lions")
     team3 = Team("Grouches")
-    
     team_set = {team1, team3}
     assert team2 in team_set
     assert len(team_set) == 2
+
+def test_result_constructor():
+    team1 = Team("Lions")
+    team2 = Team("Grouches")
+    result = Result(team1, 3, team2, 1)
+    assert repr(result) == "Result(Team(Lions) 3, Team(Grouches) 1)"
+
+def test_result_home_winner():
+    team1 = Team("Lions")
+    team2 = Team("Grouches")
+    result = Result(team1, 3, team2, 1)
+    assert result.winner() == team1
+
+def test_result_away_winner():
+    team1 = Team("Lions")
+    team2 = Team("Grouches")
+    result = Result(team1, 1, team2, 4)
+    assert result.winner() == team2
+
+def test_result_draw():
+    team1 = Team("Lions")
+    team2 = Team("Grouches")
+    result = Result(team1, 2, team2, 2)
+    assert result.winner() is None 
 
